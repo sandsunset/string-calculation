fbb68e5a116dfa5d57c1f0fe662d4c638a343bbb
Sandsunset <sejoon0606@gmail.com>
2021-04-04 17:43:30 +0900

    feat: get_coefficient

--- __init__.py
+++ __init__.py
@@ -1,6 +1,22 @@
-def execute(formula, **kwargs):
-    print(formula)
-    print(kwargs)
+import re
+
+def get_coefficient(formula:str):
+    variables = {}
+    term = ""
+    for f in formula:
+        variable = re.compile('(\W?[0-9]+)([a-zA-Z])')
+        constant_term = re.compile('(\W?[0-9])+\W')
+        term += f
+        variable = variable.match(term)
+        constant_term = constant_term.match(term)
+        if variable == None and constant_term == None:
+            continue
+        elif variable != None:
+            print(variable.group(1))
+            term = ""
+        elif constant_term != None:
+            print(constant_term.group(1))
+            term = ""
 
 if __name__ == '__main__':
-    print(__name__)
+    get_coefficient('5i-3')
