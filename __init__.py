4d2d4cb7bbb9f47498b76b246744ecd98f42313e
Sandsunset <sejoon0606@gmail.com>
2021-04-12 10:44:53 +0900

    test

--- __init__.py
+++ __init__.py
@@ -1,3 +1,6 @@
+from time import time
+start = time()
+
 import re
 
 def get_coefficient(formula:str):
@@ -7,6 +10,7 @@ def get_coefficient(formula:str):
         variable = re.compile('(\W?[0-9]+)([a-zA-Z])')
         constant_term = re.compile('(\W?[0-9])+\W')
         term += f
+
         variable = variable.match(term)
         constant_term = constant_term.match(term)
         if variable == None and constant_term == None:
@@ -19,4 +23,33 @@ def get_coefficient(formula:str):
             term = ""
 
 if __name__ == '__main__':
-    get_coefficient('5i-3')
+    get_coefficient('-5i-3x')
+    print(f"{time() - start}s")
+
+"""
+0.008~0.009
+        variable = variable.match(term)
+        if variable == None:
+            constant_term = constant_term.match(term)
+            if constant_term == None:
+                continue
+            else:
+                print(constant_term.group(1))
+                term = ""
+        else:
+            print(variable.group(1))
+            term = ""
+
+
+0.008~0.009
+        variable = variable.match(term)
+        constant_term = constant_term.match(term)
+        if variable == None and constant_term == None:
+            continue
+        elif variable != None:
+            print(variable.group(1))
+            term = ""
+        elif constant_term != None:
+            print(constant_term.group(1))
+            term = ""
+"""
