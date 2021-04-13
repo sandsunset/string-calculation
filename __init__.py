e80167997484fede3846523649fc67fe31f2a91d
Sandsunset <sejoon0606@gmail.com>
2021-04-13 11:02:51 +0900

    feat: coefficient add

--- __init__.py
+++ __init__.py
@@ -1,27 +1,45 @@
 import re
 
-def get_coefficient(formula:str) -> dict:
-    formula = formula.replace(' ','')
-    coefficients = {}
-    term = ""
+class StringCal:
+    def __init__(self, formula:str):
+        self.formula = formula.replace(' ', '')
 
-    for f in formula + ';':
-        variable = re.compile('(\W?[0-9]+)([a-zA-Z]+)')
-        constant_term = re.compile('(\W?[0-9]+)[^a-zA-Z0-9_]')
-        term += f
+    def get_coefficient(self) -> dict:
+        coefficients = {}
+        term = ""
 
-        variable = variable.match(term)
-        constant_term = constant_term.match(term)
-        if variable == None and constant_term == None:
-            continue
-        elif variable != None:
-            coefficients[variable.group(2)] = variable.group(1)
-            term = ""
-        elif constant_term != None:
-            coefficients['constant'] = constant_term.group(1)
-            term = ""
+        for f in self.formula + ';':
+            variable_term = re.compile('(\W?[0-9]*)([a-zA-Z]+)')
+            constant_term = re.compile('(\W?[0-9]+)(\W)')
+            is_coefficientOne = re.compile('(\W?)([a-zA-Z]+)')
+            term += f
 
-    return coefficients
+            variable_term = variable_term.match(term)
+            constant_term = constant_term.match(term)
+            if variable_term == None and constant_term == None:
+                continue
+            elif variable_term != None:
+                variable = variable_term.group(2)
+                coefficient = variable_term.group(1)
+                
+                if is_coefficientOne.match(variable_term.group()):
+                    coefficient += '1'
+                try:
+                    coefficients[variable] = eval(str(coefficients[variable]) + coefficient)
+                except KeyError:
+                    coefficients[variable] = int(coefficient)
+                term = ""
+            elif constant_term != None:
+                constant = constant_term.group(1)
+                
+                try:
+                    coefficients['constant'] = eval(str(coefficients['constant']) + constant)
+                except KeyError:
+                    coefficients['constant'] = int(constant)
+                term = constant_term.group(2)
+
+        return coefficients
 
 if __name__ == '__main__':
-    print(get_coefficient('2  x        -3+     5  x + 4                 '))
+    formula = StringCal('2x+1-3')
+    print(formula.get_coefficient())
